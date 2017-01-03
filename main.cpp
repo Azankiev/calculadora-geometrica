6c6a2991918b587f3fad533b73a61fb417b35dbe
Azankiev <viniciusguilty@outlook.com>
2017-01-03 00:44:10 -0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class Quadrado{
+
+    float area;
+    float perimetro;
+
+public:
+    float la;
+    int contaArea (float num);
+    int contaPerimetro (float num2);
+    float restultadoArea();
+    float resultadoPerimetro();
+};
+
+
+
+int main()
+{
+
+
+  Quadrado quad;
+
+cin>>quad.la;
+
+
+
+
+
+    return 0;
+}
